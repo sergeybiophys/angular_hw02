035125141e39e0e751b42c031243736dd4d78c01
sergeybiophys <rove2hati@gmail.com>
2021-11-29 23:54:02 +0200

    installed PrimeNG

--- hw02-app/src/app/students-list/shared/shared.module.ts
+++ hw02-app/src/app/students-list/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {ReactiveFormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [
 
     ],
     imports: [
+      CommonModule,
+      ReactiveFormsModule,
+      BrowserAnimationsModule,
         
     ],
     exports: [
